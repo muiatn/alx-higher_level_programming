d0ffe558f8555ce97b3ec9ff5dc7a50874bd4495
muiatn <muiatitus80@gmail.com>
2022-09-27 14:54:33 -0700

    tasks

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+'''
+Module for write_file function
+'''
+
+
+def write_file(filename="", text=""):
+    '''Writes a string to a file (UTF8) and returns
+     number of characters written
+     '''
+    with open(filename, 'w', encoding="utf-8") as f:
+        return f.write(text)
